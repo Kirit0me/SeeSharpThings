71480da21f0ddaeac164ae565ca3baed11b3a932
Kiritome <62535554+Kirit0me@users.noreply.github.com>
2023-06-17 19:49:16 +0530

    Add files via upload

---
+++ factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Kurukuru
+{
+    internal class Program
+    {
+        static int countzeros(int n)
+        {
+            int count = 0;
+            while (n >= 5)
+            {
+                n /= 5;
+                count += n;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int zeroes = countzeros(n);
+            Console.WriteLine("Number of trailing zeroes in {0} factorial: {1}", n, zeroes);
+        }
+    }
+}
