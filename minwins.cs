7201a06b1847a1c047c5373fe6e96671e40e84ec
Kiritome <62535554+Kirit0me@users.noreply.github.com>
2023-06-17 20:06:06 +0530

    Add files via upload

---
+++ minwins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Kurukuru
+{
+    internal class Program
+    {
+        static int minwins(int points, int matches)
+        {
+ 
+            int required_win_points = points-matches;
+
+            int minimumWins = required_win_points / 2;
+            return minimumWins;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of points required to qualify: ");
+            int X = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of matches: ");
+            int Y = int.Parse(Console.ReadLine());
+
+            int mw = minwins(X, Y);
+            Console.WriteLine("The minimum number of wins required to qualify is {0}", mw);
+        }
+
+
+    }
+}
